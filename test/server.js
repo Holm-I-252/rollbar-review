8520239b3066c443a0140aa7c49e02b316970632
Ian Holm <ianholm252@gmail.com>
2021-09-01 09:54:10 -0600

    fixed rollbar

--- test/server.js
+++ test/server.js
@@ -10,6 +10,17 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+app.get("/api/test", (req, res) => {
+  console.log(req.body);
+  if (req.body === "test") {
+    rollbar.log("correct input");
+    res.status(200).send("Correct input");
+  } else {
+    rollbar.error("incorrect input");
+    res.status(200).send("incorrect input");
+  }
+});
+
 let port = process.env.PORT || 2525;
 
 app.listen(port, () => {
