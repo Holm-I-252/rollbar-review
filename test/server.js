e6642aef8831ca2b0d70b528ad61fa1bb5f6cfc2
Ian Holm <ianholm252@gmail.com>
2021-09-01 09:39:30 -0600

    fixed port

--- test/server.js
+++ test/server.js
@@ -9,3 +9,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
+let port = process.env.PORT || 2525;
+
+app.listen(port, () => {
+  console.log(`running on ${port}`);
+});
